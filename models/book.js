fe70a6504984389323d8f1f257264a95fb19e3f0
George Travis <wewerelikesharks@gmail.com>
2020-04-19 10:53:19 -0700

    model set up correctly, tested with postman

--- models/book.js
+++ models/book.js
@@ -1,13 +1,13 @@
-
-
 const mongoose = require(`mongoose`);
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    synopsis: String,
-    date: { type: Date, default: Date.now }
+  title: { type: String, required: true },
+  authors: { type: [String], required: true },
+  description: { type: String },
+  link: { type: String, required: true },
+  image: { type: String, required: true },
+  date: { type: Date, default: Date.now },
 });
 
 const Book = mongoose.model(`Book`, bookSchema);
