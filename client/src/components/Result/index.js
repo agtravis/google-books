045195985300390276eede17df459755cba12c5f
George Travis <wewerelikesharks@gmail.com>
2020-04-19 22:34:12 -0700

    succesfully searches for some titles

--- client/src/components/Result/index.js
+++ client/src/components/Result/index.js
@@ -4,7 +4,12 @@ import './style.css';
 function Result(props) {
   return (
     <div>
-      <p>{props.title}</p>
+      <h3>{props.title}</h3>
+      <p>{props.authors[0]}</p>
+      <img src={props.image} alt={props.title} />
+      <a href={props.link}>{props.link}</a>
+      <p>{props.description}</p>
+      <hr />
     </div>
   );
 }
--- client/src/components/ResultsContainer/index.js
+++ client/src/components/ResultsContainer/index.js
@@ -6,7 +6,17 @@ function ResultsContainer(props) {
   return (
     <div>
       {props.array.map((book, index) => {
-        return <Result key={index} id={index} title={book.volumeInfo.title} />;
+        return (
+          <Result
+            key={index}
+            id={index}
+            title={book.volumeInfo.title}
+            authors={book.volumeInfo.authors}
+            description={book.volumeInfo.description}
+            image={book.volumeInfo.imageLinks.thumbnail}
+            link={book.volumeInfo.infoLink}
+          />
+        );
       })}
     </div>
   );
