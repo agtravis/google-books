1e9bacf22ab0fc148ae1b40df5ae95c31e2e0a38
George Travis <wewerelikesharks@gmail.com>
2022-04-16 21:05:57 -0700

    test push to heroku

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -7,7 +7,7 @@ function Header() {
       <div className="container">
         <div className="row">
           <div id="header-title" className="col s12 m12 l12">
-            <h1>George's Books</h1>
+            <h1>George's Books!</h1>
           </div>
         </div>
       </div>
